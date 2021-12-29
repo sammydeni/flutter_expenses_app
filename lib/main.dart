9e10765ac9b2386064ff6ed5a46b17ead1307b08
Samuele Denicola <samueledenicola01@gmail.com>
2021-12-29 12:28:48 +0100

    string interpolation and import packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import './transaction.dart';
 
 void main() {
@@ -67,7 +69,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -86,7 +88,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMEd().format(tx.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
