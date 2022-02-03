135dc656f8e7203f9a75cc692faceb9b64bbc9f2
sammydeni <samueledenicola01@gmail.com>
2022-02-03 17:40:08 +0100

    added the chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_expenses_app/widgets/chart.dart';
 import 'package:flutter_expenses_app/widgets/new_transaction.dart';
 import 'package:flutter_expenses_app/widgets/transaction_list.dart';
 import './models/transaction.dart';
@@ -19,12 +20,12 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
               .copyWith(secondary: Colors.orange),
           textTheme: const TextTheme(
-              bodyText2: TextStyle(color: Colors.orange),
+              bodyText2: TextStyle(color: Colors.black),
               headline2: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold)),
-          fontFamily: 'Quicksand',
+                color: Colors.black,
+                fontSize: 18,
+                fontFamily: 'Quicksand',
+              )),
           appBarTheme: AppBarTheme(
               titleTextStyle: ThemeData.light()
                   .textTheme
@@ -58,6 +59,16 @@ class _MyHomePageState extends State<MyHomePage> {
     //   date: DateTime.now(),
     // ),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -101,17 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Theme.of(context).colorScheme.primary,
-                child: Text(
-                  'Chart',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions)
           ],
         ),
